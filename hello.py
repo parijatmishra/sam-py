98e7d1f199bf89d886740cf1c22b086a5cb8e374
Parijat Mishra <parijat.mishra@gmail.com>
2018-01-29 11:56:23 -0800

    Added hello.py and updated template.yml

---
+++ hello.py
@@ -0,0 +1,12 @@
+import json
+import datetime
+
+
+def handler(event, context):
+    data = {
+        'output': 'Hello' + event['pathParameters']['name'],
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
